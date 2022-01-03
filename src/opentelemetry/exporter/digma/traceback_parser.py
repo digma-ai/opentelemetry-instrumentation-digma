56bc87db69cdc19937767ef83ccf9246b2eb3463
Shay Keren <shay.keren@gmail.com>
2022-01-03 22:09:02 +0200

    fix parser to support no code line in frame + repeat frame

--- src/opentelemetry/exporter/digma/traceback_parser.py
+++ src/opentelemetry/exporter/digma/traceback_parser.py
@@ -1,7 +1,8 @@
 import os
+import re
 import site
 from dataclasses import dataclass
-from typing import List
+from typing import List, Tuple
 
 import sys
 from os import path
@@ -16,6 +17,7 @@ class TracebackFrame:
     func_name: str
     line: str
     path: str
+    repeat: int = 0  # when repeat > 0, means line repeated {repeat} more times
 
 
 @dataclass
@@ -25,46 +27,76 @@ class TracebackFrameStack:
 
 
 class TracebackParser:
+    FRAME_FILE_PATTERN = '\s\sFile\s".*",\sline [0-9]+,\sin\s.*'
+    FRAME_CODE_LINE_PATTERN = '(\s\s\s\s.*).+'
+    FRAME_LOCALS_LINE_PATTERN = '\s\s\s\s.*\s=\s.*'
+    FRAME_REPEAT_PATTERN = '^\s\s\[Previous line repeated ([0-9]+) more time(s?)\]$'
 
     @staticmethod
     def split(arr: List[str], size: int):
         for idx in range(math.floor(len(arr) / size)):
             yield arr[idx * size: idx * size + size]
 
-    def error_flow_parser(self, stacktrace: str) -> List[TracebackFrame]:
-        lines = stacktrace.splitlines()[1: -1]
-        frames = iter(self.split([line.strip() for line in lines], 2))
-        error_frames = []
-        for frame in frames:
-            error_frames.append(self._frame_parser(frame))
-        return error_frames
-
     def parse_error_flow_stacks(self, stacktrace: str) -> List[TracebackFrameStack]:
         frames: List[TracebackFrame] = []
         stacks: List[TracebackFrameStack] = []
-        first_frame_line = None
+
+        lines = stacktrace.splitlines()
+        line_num = 0
         skip_to_next_traceback = True
-        for idx, line in enumerate(stacktrace.splitlines()):
-            if skip_to_next_traceback:  # skip lines until next traceback
-                if line.startswith('Traceback'):
+        while line_num < len(lines):
+            line = lines[line_num]
+            if skip_to_next_traceback:
+                if line == 'Traceback (most recent call last):':
                     skip_to_next_traceback = False
                     frames = []
+                line_num += 1
                 continue
+            if bool(re.match(self.FRAME_FILE_PATTERN, line)):
+                code_line = None
+                repeat = 0
+                fullpath, func_name, code_line_num, normalize_path = self.file_line_parser(line)
+                line_num += 1
+                line = lines[line_num]
+                if bool(re.match(self.FRAME_CODE_LINE_PATTERN, line)):
+                    code_line = line.strip()
+                    line_num += 1
+                    line = lines[line_num]
 
-            if line.strip().startswith('File'):  # start a new frame
-                first_frame_line = line
-            else:
-                if first_frame_line:  # in the middle of a frame
-                    frames.append(self._frame_parser([first_frame_line, line]))
-                    first_frame_line = None
-                else:  # last line of a traceback ( exception type)
-                    stacks.append(
-                        TracebackFrameStack(frames=frames, exception_type=line.split(':')[0]))
-                    skip_to_next_traceback = True
+                if bool(re.match(self.FRAME_CODE_LINE_PATTERN, line)):
+                    line_num += 1
+
+                match = re.search(self.FRAME_REPEAT_PATTERN, line)
+                if match:
+                    line_num += 1
+                    repeat = int(match.group(1))
+                frames.append(TracebackFrame(fullpath=fullpath,
+                                             line_num=code_line_num,
+                                             func_name=func_name,
+                                             line=code_line,
+                                             path=normalize_path,
+                                             repeat=repeat))
+                continue
+            if frames:
+                # if frames are empty,
+                # it means done processing all current traceback details
+                # continue until the next traceback
+                stacks.append(
+                    TracebackFrameStack(frames=frames, exception_type=line.split(':')[0]))
+                skip_to_next_traceback = True
+            line_num += 1
         return stacks
 
+    def file_line_parser(self, line) -> Tuple[str, str, int, str]:
+        split_line = line.split(',')
+        fullpath = split_line[0].strip()[6:-1]
+        line_num = int(split_line[1].strip()[5:])
+        func_name = split_line[2].strip()[3:]
+        normalize_path = self._forward_slash_for_paths(self._file_path_normalizer(fullpath))
+        return fullpath, func_name, line_num, normalize_path
+
     @staticmethod
-    def _forward_slash_for_paths(file_path: str) -> str:
+    def _forward_slash_for_paths(file_path: str) -> str:  # todo: shay why cannot we use os definitions?
         return file_path.replace('\\', '/')
 
     @staticmethod
@@ -86,12 +118,3 @@ class TracebackParser:
                 normalize_path = file_path[len(prefix_parent) + 1:]
                 return normalize_path
         return file_path
-
-    def _frame_parser(self, lines: List[str]) -> TracebackFrame:
-        split_line = lines[0].split(',')
-        fullpath = split_line[0].strip()[6:-1]
-        line_num = int(split_line[1].strip()[5:])
-        func_name = split_line[2].strip()[3:]
-        line = lines[1].strip()
-        path = self._forward_slash_for_paths(self._file_path_normalizer(fullpath))
-        return TracebackFrame(fullpath=fullpath, line_num=line_num, func_name=func_name, line=line, path=path)
--- tests/test_traceback_parser.py
+++ tests/test_traceback_parser.py
@@ -43,3 +43,45 @@ class TracebackParserTest(TestCase):
 
         frame_stacks = TracebackParser().parse_error_flow_stacks(stacktrace=exc_stacktrace)
         self.assertEqual(len(frame_stacks), 1)
+
+    def test_traceback_with_frame_with_no_line(self):
+        try:
+            print(eval("100/x", {"x": 0}))
+        except:
+            exc_stacktrace = traceback.format_exc()
+
+        frame_stacks = TracebackParser().parse_error_flow_stacks(stacktrace=exc_stacktrace)
+        self.assertEqual(len(frame_stacks), 1)
+        self.assertIsNone(frame_stacks[0].frames[1].line)
+
+    # @pytest.mark.parametrize("recursion_depth,expected", [(4, 1), (10, 7)])
+    def test_recursion(self):
+
+        def recursion_call(depth: int):
+            if depth != 0:
+                print(depth)
+                return recursion_call(depth - 1)
+            else:
+                raise ValueError('?')
+
+        recursion_depth = 4
+        expected = 1  # check _RECURSIVE_CUTOFF under traceback.py  equals to 3
+        try:
+            recursion_call(recursion_depth)
+        except:
+            exc_stacktrace = traceback.format_exc()
+
+        frame_stacks = TracebackParser().parse_error_flow_stacks(stacktrace=exc_stacktrace)
+
+        self.assertEqual(frame_stacks[0].frames[3].repeat, expected)
+
+        recursion_depth = 10
+        expected = 7  # check _RECURSIVE_CUTOFF under traceback.py equals to 3
+        try:
+            recursion_call(recursion_depth)
+        except:
+            exc_stacktrace = traceback.format_exc()
+
+        frame_stacks = TracebackParser().parse_error_flow_stacks(stacktrace=exc_stacktrace)
+
+        self.assertEqual(frame_stacks[0].frames[3].repeat, expected)  # check
