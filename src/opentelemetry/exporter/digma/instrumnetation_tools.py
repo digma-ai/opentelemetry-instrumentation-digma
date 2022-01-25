1368afe7d6e8f38254311ffa49468c77d8c2a9f6
Roni Dover <roni.dover@gmail.com>
2022-01-24 21:24:19 -0800

    new parameter extraction

--- src/opentelemetry/exporter/digma/instrumnetation_tools.py
+++ src/opentelemetry/exporter/digma/instrumnetation_tools.py
@@ -21,23 +21,20 @@ def record_exception(
     if not ex:
         return None
 
-    tbe = traceback.TracebackException.from_exception(
-        ex, limit=None, capture_locals=True)
-
     _attributes = {
-        'exception.locals': _get_locals_statistics(tbe)
+        'exception.locals': _get_locals_statistics(ex)
     }
     if attributes:
         _attributes.update(attributes)
     return default_record_exception(self, exception, _attributes, timestamp, escaped)
 
 
-def _get_frame_id(frame: traceback.FrameSummary):
-    return f"{frame.filename}/{frame.name}:{frame.lineno}"
+def _get_frame_id(frame,line_number):
+    return f"{frame.f_code.co_filename}/{frame.f_code.co_name}:{line_number}"
 
 
 def _extract_local_info(local_name, local_obj) -> dict:
-    local_stats = {"length": "",
+    local_stats = {"length": 0,
                    "is_none": (local_obj == 'None'),
                    "type": str(type(local_obj)),
                    "value": ""}
@@ -57,6 +54,26 @@ def _extract_class_info(local_name, local_obj):
     if match:
         return match.group(1)
     return ""
+
+
+def _extract_frames_info(ex: Exception):
+    if ex.__cause__ is not None:
+        yield from _extract_frames_info(ex.__cause__)
+    elif ex.__context__ is not None and not ex.__suppress_context__:
+        yield from _extract_frames_info(ex.__context__)
+
+    for frame, line in traceback.walk_tb(ex.__traceback__):
+        locals_stats = {}
+        frame_class = ""
+        if frame.f_locals:
+            for local in frame.f_locals:
+                localobj = frame.f_locals[local]
+                if local == 'self':
+                    frame_class = type(localobj).__name__
+                else:
+                    locals_stats[local] = _extract_local_info(local, localobj)
+
+            yield _get_frame_id(frame, line), {"class": frame_class, "locals": locals_stats}
 
 
 def _extra_frame_info(tbe: traceback.TracebackException):
@@ -80,11 +97,10 @@ def _extra_frame_info(tbe: traceback.TracebackException):
             yield _get_frame_id(frame), {"class": frame_class, "locals": locals_stats}
 
 
-def _get_locals_statistics(tbe: traceback.TracebackException):
+def _get_locals_statistics(tbe: traceback):
     # locals = list(_recursively_extract_locals(tbe))
-    frame_extra_info = dict(_extra_frame_info(tbe))
+    frame_extra_info = dict(_extract_frames_info(tbe))
     return json.dumps(frame_extra_info)
-
 
 def extend_otel_exception_recording():
     Span.record_exception = record_exception
