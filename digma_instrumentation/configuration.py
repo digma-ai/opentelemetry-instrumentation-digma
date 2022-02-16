2c10090635237cd41e818976900f8f5a28bfc47e
asafc64 <asafc64@gmail.com>
2022-02-16 18:38:27 +0200

    Modify resource attributes (#19)

--- digma_instrumentation/configuration.py
+++ digma_instrumentation/configuration.py
@@ -2,6 +2,7 @@ import inspect
 import os
 from importlib import util
 
+from opentelemetry.attributes import BoundedAttributes
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource, DEPLOYMENT_ENVIRONMENT
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
@@ -15,14 +16,23 @@ class Configuration:
 
     def __init__(self):
         self.environment = os.environ.get('ENVIRONMENT', 'DEV')
-        self.commitId = os.environ.get('GIT_COMMIT_ID', '')
-        self.traceablePaths = []
+        self.commit_id = os.environ.get('GIT_COMMIT_ID', '')
+        self.this_package_root = None
+        self.other_package_roots = []
 
-    def trace_this_package(self, root='./'):
+    def set_environment(self, value: str) -> 'Configuration':
+        self.environment = value
+        return self
+
+    def set_commit_id(self, value: str) -> 'Configuration':
+        self.commit_id = value
+        return self
+
+    def trace_this_package(self, root='./') -> 'Configuration':
         current_frame = inspect.currentframe()
         caller_frame = inspect.getouterframes(current_frame)[1]
         package_root = os.path.realpath(os.path.join(os.path.dirname(caller_frame.filename), root))
-        self.traceablePaths.append(package_root.replace('\\', '/'))
+        self.this_package_root = package_root.replace('\\', '/')
         return self
 
     def trace_package(self, module_name: str) -> 'Configuration':
@@ -30,16 +40,21 @@ class Configuration:
         if not spec:
             raise ValueError(f'Module {module_name} was not found')
         for path in spec.submodule_search_locations:
-            self.traceablePaths.append(path.replace('\\', '/'))
+            self.other_package_roots.append(path.replace('\\', '/'))
         return self
 
     @property
     def resource(self):
         return Resource(attributes={
             DEPLOYMENT_ENVIRONMENT: self.environment,
-            'commitId': self.commitId,
-            'traceableFilePaths': self.traceablePaths,
-            'workingDirectory': os.getcwd().replace('\\', '/')
+            'commit_id': self.commit_id,
+            'paths.this_package_root': self.this_package_root,
+            'paths.other_package_roots': self.other_package_roots,
+            'paths.venv_root': os.getenv('VIRTUAL_ENV'),
+            'paths.working_directory': os.getcwd().replace('\\', '/'),
+            'a': BoundedAttributes(attributes={
+                'b': 1
+            })
         })
 
     @property
