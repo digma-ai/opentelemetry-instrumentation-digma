86c7ed9460fb2ad18b44cb0b0acfc2dd73f38ba1
Shay Keren <shay.keren@gmail.com>
2022-05-03 16:30:25 +0300

    digma semconv changes

--- src/opentelemetry/instrumentation/digma/resource_attributes.py
+++ src/opentelemetry/instrumentation/digma/resource_attributes.py
@@ -1,8 +1,8 @@
 COMMIT_ID = 'scm.commit.id'
-WORKING_DIRECTORY = 'paths.working_directory'  # 'paths.working_directory'
-THIS_PACKAGE_ROOT = 'paths.this_package_root'  # 'paths.python.packages.main.root'
-OTHER_PACKAGE_ROOTS = 'paths.other_package_roots'  # 'paths.python.packages.traceable.roots'
-VENV_ROOT = 'paths.venv_root'  # 'paths.python.venv.root'
+WORKING_DIRECTORY = 'paths.working_directory'
+THIS_PACKAGE_ROOT = 'paths.python.packages.main.root'
+OTHER_PACKAGE_ROOTS = 'paths.python.packages.traceable.roots'
+VENV_ROOT = 'paths.python.venv.root'
 
 
 
